ceb047bf67a29d832ec15dacb7151584f37fd0c0
Tong Yin <tongyin@Tongs-MacBook-Air.local>
2025-11-09 15:50:32 -0800

    vite-react-tailwind-starter

---
+++ prettier.config.js
@@ -0,0 +1,12 @@
+export default {
+  semi: true,
+  singleQuote: false,
+  tabWidth: 2,
+  trailingComma: "es5",
+  printWidth: 80,
+  arrowParens: "always",
+  endOfLine: "lf",
+  bracketSpacing: true,
+  jsxSingleQuote: false,
+  plugins: ["prettier-plugin-tailwindcss"],
+};
---
+++ src/App.tsx
@@ -0,0 +1,5 @@
+function App() {
+  return <div className="text-2xl">Hello World</div>;
+}
+
+export default App;
